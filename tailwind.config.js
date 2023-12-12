21ac93e422eac962cd445c00c89aecbca5297e9d
KevZ <kevinjgo121@gmail.com>
2023-12-12 15:25:59 -0600

    Footer created, added svg logo to the public folder, new color for the background of the footer added on the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        footerColor: '#768692',
+      }
+    },
   },
   variants: {
     extend: {},
