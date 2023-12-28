f86f17ccc889b457137973520befcef75eeecafb
Molina60217N2 <molinasalas16@gmail.com>
2023-12-28 11:43:40 -0600

    Fix de comillas dobles

--- components/homepage/QuienesSomos.jsx
+++ components/homepage/QuienesSomos.jsx
@@ -5,7 +5,7 @@ import styles from "./QuienesSomos.module.css"
 export default function QuienesSomos(){
     return(
         <section className="container max-w-full pt-16 pb-16 text-center">
-            <h2 className="font-bold max-[1024px]:text-2xl  lg:text-5xl">¿Quiénes Somos?</h2>
+            <h2 className="font-bold max-[1024px]:text-2xl lg:text-5xl">¿Quiénes Somos?</h2>
             <p className="hidden md:block w-2/4 text-xl mx-auto mt-3 text-center font-normal not-italic decoration-black">En "El Arte de Forjar el Hierro", somos artesanos apasionados dedicados a transformar tus ideas en obras de arte duraderas.</p>
             <div className="pt-8 container max-w-full flex justify-center">
             <div className={`text-left block items-center m-auto p-10 text-base font-normal bg-white md:text-lg lg:text-2xl rounded-xl max-[1024px]:w-11/12 lg:w-4/12 xl:w-4/12 2xl:w-1/2 ${styles.textcontainer}`}>
@@ -16,7 +16,7 @@ export default function QuienesSomos(){
                 <br/>
                 <p className="font-bold">Mision:</p>
                 <br/>
-                <p className="">En “El Arte de Forjar el Hierro”, nos comprometemos a fusionar la tradición artesanal con la innovación, creando productos únicos y personalizados que superen las expectativas de nuestros clientes. Nos especializamos en la fabricación de portones, candelabros, muebles y más, utilizando técnicas de forja de alta calidad.</p>
+                <p className="">En &quot;El Arte de Forjar el Hierro&quot;, nos comprometemos a fusionar la tradición artesanal con la innovación, creando productos únicos y personalizados que superen las expectativas de nuestros clientes. Nos especializamos en la fabricación de portones, candelabros, muebles y más, utilizando técnicas de forja de alta calidad.</p>
             </div>
             <div className="w-max pr-5 hidden lg:block">
                 <Image
