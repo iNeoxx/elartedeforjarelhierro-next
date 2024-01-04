9a81e223d313d04951f445ceb2594526217660c8
Molina60217N2 <molinasalas16@gmail.com>
2024-01-04 15:43:35 -0600

    No autoplay

--- components/CarouselBlog.jsx
+++ components/CarouselBlog.jsx
@@ -5,9 +5,9 @@ import "react-image-gallery/styles/css/image-gallery.css";
 export default function CarouselBlog({ images }) {
   const urlImages = images.map(image => ({
     original: absoluteUrl(image.uri.url),
-    thumbnail:absoluteUrl(image.uri.url)
+    thumbnail:absoluteUrl(image.uri.url),
   }))
   return (
-     <ImageGallery items={urlImages} showPlayButton={false} showThumbnails={false} autoPlay={true}/>
+     <ImageGallery items={urlImages} showPlayButton={false} showThumbnails={false} autoPlay={false} showFullscreenButton={false}/>
     );
   }
