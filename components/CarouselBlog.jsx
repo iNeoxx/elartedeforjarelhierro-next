59d66ef3b7d4ab76e9c609fc4131d6ae6d1d1a10
Molina60217N2 <molinasalas16@gmail.com>
2024-01-04 14:45:24 -0600

    New Carousel for testing

--- components/CarouselBlog.jsx
+++ components/CarouselBlog.jsx
@@ -2,12 +2,12 @@ import { absoluteUrl } from "../lib/utils";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-export default function Carousel({ images }) {
+export default function CarouselBlog({ images }) {
   const urlImages = images.map(image => ({
     original: absoluteUrl(image.uri.url),
     thumbnail:absoluteUrl(image.uri.url)
   }))
   return (
-     <ImageGallery items={urlImages} showPlayButton={false}/>
+     <ImageGallery items={urlImages} showPlayButton={false} showThumbnails={false} autoPlay={true}/>
     );
   }
