c4571327326dbb06076b0ff6958ec94e595036d4
Hilder Ordoñez <44181928+iNeoxx@users.noreply.github.com>
2025-09-11 17:29:25 -0600

    Update Carousel.jsx

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -7,38 +7,35 @@ import { StaticBLurDataUrl } from "@/utils/staticBlurDataUrl";
 
 const ImgCarousel = ({ ...props }) => { 
 
+  // Tema personalizado para cambiar el color de los controles de navegación
+  const customTheme = {
+    control: {
+      base: "inline-flex h-8 w-8 items-center justify-center rounded-full bg-[#497EDA] group-hover:bg-blue-700 group-focus:outline-none group-focus:ring-4 group-focus:ring-blue-300 dark:bg-blue-500 dark:group-hover:bg-blue-600 dark:group-focus:ring-blue-800 sm:h-10 sm:w-10",
+      icon: "h-5 w-5 text-white dark:text-white sm:h-6 sm:w-6"
+    }
+  };
+
   return (
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-unit-96 w-[100%] m-auto">
-      <Carousel slide={false}>
-      {
-        props.images.map((image, key)=> (
-          <Image
-          key={key}
-          src={absoluteUrl(image.uri.url)}
-          alt={image.resourceIdObjMeta.alt}
-          width={700}
-          height={700}
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-          placeholder="blur"
-          blurDataURL={StaticBLurDataUrl()}
-          />
-        ))
-      }
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-unit-96 w-[100%] m-auto">
+      <Carousel 
+        slide={false}
+        theme={customTheme}
+      >
+        {props.images.map((image, key) => (
+          <div key={key} className="relative w-full h-full">
+            <Image
+              src={absoluteUrl(image.uri.url)}
+              alt={image.resourceIdObjMeta.alt}
+              fill
+              className="rounded-2xl object-contain duration-500"
+              placeholder="blur"
+              blurDataURL={StaticBLurDataUrl()}
+            />
+          </div>
+        ))}
       </Carousel>
     </div>
-     
   );
 }
 
 export default ImgCarousel;
-{/* {
-        props.images.map((image)=> (
-          <Image
-          src={absoluteUrl(image.uri.url)}
-          alt={image.resourceIdObjMeta.alt}
-          width={700}
-          height={700}
-          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-          />
-        ))
-      } */}
