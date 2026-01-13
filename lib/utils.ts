f82ee1a9b65e56936c506040b171233c0c1b60d7
Hilder Ordoñez <44181928+iNeoxx@users.noreply.github.com>
2026-01-12 18:13:54 -0600

    Refactor absoluteUrl for cleaner URL handling
    Refactor absoluteUrl function to handle base URL and path more cleanly.

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,8 +8,15 @@ export function formatDate(input: string): string {
 }
 
 export function absoluteUrl(input: string) {
-  return `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${input}`
+  const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || '';
+  const path = input.startsWith('/') ? input : `/${input}`;
+  
+  // Elimina la barra final de baseUrl si existe
+  const cleanBaseUrl = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+  
+  return `${cleanBaseUrl}${path}`;
 }
+
 export function isRelative(url: string) {
   return !new RegExp("^(?:[a-z]+:)?//", "i").test(url)
 }
