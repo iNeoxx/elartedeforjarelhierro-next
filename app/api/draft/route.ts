2ef8b5e9c14fc8a5ca58fae8b3be65dfb1cc9773
iNeoxx <gmdneoxx1@gmail.com>
2026-02-03 01:19:19 -0600

    revert changes next stable version

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,9 +1,7 @@
-import { NextRequest } from "next/server"
-import { enableDraftMode } from "next-drupal/draft"
 import { drupal } from "@/lib/drupal"
+import { enableDraftMode } from "next-drupal/draft"
+import type { NextRequest } from "next/server"
 
-export async function GET(request: NextRequest) {
-  // Forzamos el request a 'any' para evitar el conflicto de versiones de NextRequest
-  // entre tu proyecto y la dependencia next-drupal.
-  return await enableDraftMode(request as any, drupal)
+export async function GET(request: NextRequest): Promise<Response | never> {
+  return await enableDraftMode(request, drupal)
 }
